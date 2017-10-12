38c941f97aa44d1e4dcfe98e6fffb562badda13d
dwt0317 <dwt0317@163.com>
2017-10-12 10:21:42 +0800

    fix dir error

--- ctr/constants.py
+++ ctr/constants.py
@@ -1,4 +1,4 @@
 
-impression_path = "/sad/impression/"
-click_path = "/sad/click/"
-search_path = "/sad/search/"
+impression_path = "/sad/impression"
+click_path = "/sad/click"
+search_path = "/sad/search"
